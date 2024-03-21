01cf2d9fcbf243a79c5068a04dcd8824e586e9fe
colinroberts-data-analytics-portfolio <colin@robertsdatasolutions.org>
2024-03-21 00:17:57 -0400

    Upload Complete

---
+++ PyBank/main.py
@@ -0,0 +1,62 @@
+import csv 
+
+
+# Set_input_/ output_files ----check_Readme_reference---------------------------------------
+csv_file = r"C:\Users\colin\Desktop\Python_Challenge\Python-Challenge\PyBank\Resources\budget_data.csv"
+   
+
+output_text_file_path = r"C:\Users\colin\Desktop\Python_Challenge\Python-Challenge\PyBank\analysis\budget_outcome.txt"
+
+# set_varibales_for_questions-----------check_Readme_reference---------------------------------------------
+total_number_of_months = 0
+net_total = 0
+previous_profit_loss = 0
+profit_losses_changes = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 0]
+
+# csv_reading_and_scan_rows -----------class_notes------------------------------
+with open(csv_file) as cf:
+    csvscanner = csv.reader(cf, delimiter=",")
+    next(csvscanner)  
+    for row in csvscanner:
+        # Find_total_months--------class_notes----------
+        total_number_of_months += 1
+        net_total += int(row[1])
+        
+        # Find change_in_profit_loss-------------------class_notes------------------------
+        profit_loss_change = int(row[1]) - previous_profit_loss
+        profit_losses_changes.append(profit_loss_change)
+        previous_profit_loss = int(row[1])
+        
+        # Find_greatest_increase-and_decrease_--------check_Readme_reference------------------------------------
+        if profit_loss_change > greatest_increase[1]:
+            greatest_increase = [row[0], profit_loss_change]
+        if profit_loss_change < greatest_decrease[1]:
+            greatest_decrease = [row[0], profit_loss_change]
+
+# Find_avg_change----------check_Readme_reference------------------------------------------------------------
+avg_change = round(sum(profit_losses_changes[1:]) / (total_number_of_months - 1), 2)
+
+# Format_Budget_Outcome_results--------check_Readme_reference------------------------------------------------------------
+budget_outcome = f"""
+----------------------------
+Financial Analysis
+-----------------------------
+Total Months: {total_number_of_months}
+Total: ${net_total}
+Average Change: ${avg_change}
+Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})
+Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})
+
+-------------------------------
+"""
+
+# Print analysis in terminal---------class_notes----------------------------------------------
+print(budget_outcome)
+
+# Export_Budget_Outcome_text_file--------class_notes-------------------------------------------------
+with open(output_text_file_path, "w") as PyBank_txtfile:
+    PyBank_txtfile.write(budget_outcome)
+
+print("Congratulations, it's Complete... check file budget_outcome.txt")
---
+++ PyPoll/main.py
@@ -0,0 +1,58 @@
+
+import csv
+
+# Set_input_/ output_files-----------check_Readme_reference---------------------------------------------
+csv_file = r"C:\Users\colin\Desktop\Python_Challenge\Python-Challenge\PyPoll\Resources\election_data.csv"
+
+output_text_file_path = r"C:\Users\colin\Desktop\Python_Challenge\Python-Challenge\PyPoll\analysis\voting_outcome.txt"
+
+# set_varibales_for_questions----------check_Readme_reference---------------------------------------------------------
+total_votes_casted = 0
+candidates_pos = {}
+candidate_winner = ""
+votes_4_winner = 0
+
+# csv_reading_and_scan_rows---------class_notes----------------------------------------------
+with open(csv_file) as cf:
+    csvscan = csv.reader(cf, delimiter=",")
+    next(csvscan)  
+    for row in csvscan:
+        # total votes------class_notes---------------------------
+        total_votes_casted += 1
+        
+        # votes per candidate-----------class notes-----------------------------
+        candidate_name = row[2]
+        if candidate_name in candidates_pos:
+            candidates_pos[candidate_name] += 1
+        else:
+            candidates_pos[candidate_name] = 1
+
+# Find percentage of votes per candidate & find the winner--------------check_Readme_reference-------------------------
+results = ""
+for candidate, votes in candidates_pos.items():
+    percentage = (votes / total_votes_casted) * 100
+    results += f"{candidate}: {percentage:.3f}% ({votes})\n"
+    if votes > votes_4_winner:
+        candidate_winner = candidate
+        votes_4_winner = votes
+
+# Format_voting_outcome_results----------check_Readme_reference------------------------------------------------------
+voting_outcome = f"""
+Election Results
+-------------------------
+Total Votes: {total_votes_casted}
+-------------------------
+{results}
+-------------------------
+Winner: {candidate_winner}
+-------------------------
+"""
+
+# Print analysis in terminal----------class_notes-----------------------------
+print(voting_outcome)
+
+# Export analysis results to a text file--------class_notes--------------------------------------------
+with open(output_text_file_path, "w") as PyPoll_txtfile:
+    PyPoll_txtfile.write(voting_outcome)
+
+print("Congratulations, it's Complete... check file voting_outcome.txt")
