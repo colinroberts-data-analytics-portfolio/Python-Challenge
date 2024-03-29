1e9b96d2d4133ac4078c386445f5d8dd327fc104
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-03-28 21:28:24 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,7 @@
 import csv
 
 # Set_input_/ output_files-----------check_Readme_reference---------------------------------------------
-csv_file = "Resources/election_data.csv"
+csv_file = "../Resources/election_data.csv"
 
 output_text_file_path = "analysis/voting_outcome.txt"
 
